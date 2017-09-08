232485edd9bf825d0083df79f730428094708de7
Laxman <laxman.rana@hpe.com>
2017-09-08 22:21:49 +0530

    custom property binding of directive

--- basic-directive/src/app/better-highlight/better-highlight.directive.ts
+++ basic-directive/src/app/better-highlight/better-highlight.directive.ts
@@ -1,25 +1,35 @@
-import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive,
+   OnInit, 
+   Renderer2, 
+   ElementRef, 
+   HostListener, 
+   HostBinding,
+   Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
+  @Input() defaultBackgroundColor : string = "transparent";
+  @Input() highlightColor: string = "blue";
+
   // Host Binding takes property which we need to bind. Use camel case since DOM doesn't understand -.
-  @HostBinding('style.backgroundColor') backgroundColor : string = "transparent";
+  @HostBinding('style.backgroundColor') backgroundColor : string;
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
     //this.renderer.setStyle(this.elementRef.nativeElement,"background-color","blue");
+    this.backgroundColor = this.defaultBackgroundColor;
   }
-
+  // Host Listener takes events of element
   @HostListener('mouseenter') mouseEnter(eventData: Event) {
     //this.renderer.setStyle(this.elementRef.nativeElement,"background-color","blue");
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
     //this.renderer.setStyle(this.elementRef.nativeElement,"background-color","transparent");
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultBackgroundColor;
   }
 }
