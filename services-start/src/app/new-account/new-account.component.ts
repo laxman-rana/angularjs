c7a81f13e5011230b63e768b759d477e7cf1e653
Laxman <laxman.rana@hpe.com>
2017-09-09 13:18:28 +0530

    created accounts service and used in other components. Right now every component will have their own account service instance rather than common instance.

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,24 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoggingService } from "../logging.service";
+import { AccountsService } from "../accounts.service";
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService, AccountsService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+ 
 
   // Here we are telling angular to provide the instance of LoggingSerice to us. By this way angular identifies
   //that angular needs to inject loggingservice. But how angular will craete it? For that we need to add Providers property in Component decorator.
-  constructor(private loggingService: LoggingService){}
+  constructor(private loggingService: LoggingService, private accountService: AccountsService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-   this.loggingService.logStatusChange(accountStatus);
+    this.accountService.onAccountAdded(accountName, accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
